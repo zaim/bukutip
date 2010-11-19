fa53a151ccb40ef2deecb6c649c468328a99b943
Zaim Bakar <abuzaim@gmail.com>
2010-11-20 00:52:10 +0800

    Refactored UI code
    HTML/CSS more semantic, usage of HTML5 tags, refactored the JS

--- static/js/bookshop.js
+++ static/js/bookshop.js
@@ -1,78 +1,90 @@
 (function($){
 
-	$.Bookshop = {};
+	$.Bookshop = {
+		"init": function(book) {
+			var	table = $('#book-'+book.isbn+'-prices'),
+				rows = table.find('tbody>tr'),
+				total = rows.length,
+				loaded = 0,
+				lowestPrice = null,
+				lowestElem = null;
 
-	$.Bookshop.init = function(isbn, title, author){
-		var	rows = $('#book-'+isbn+'-prices tbody>tr'),
-			loaded = 0,
-			total = rows.length,
-			lowest_price = null,
-			lowest_elem = null,
-			completed = function(){
-				$(lowest_elem).addClass('lowest').find('th').append('<span>lowest</span>');
-				$('#wait').remove();
-			};
+			var	completed = function() {
+					$(lowestElem).addClass('lowest').find('th').append(' <span>lowest</span>');
+					$('#price-loader-help').text("Click on a Bookstore to go to the book page");
+				};
 
-		rows.each(function(){
-			var	name = this.className,
-				data = {
-					"isbn": isbn,
-					"title": title,
-					"author": author,
-					"shop": name
-				},
-				row = $(this),
-				ptd = row.find('td');
-
-			ptd.addClass('loading').text('loading');
-
-			$.get('/price', data, function(js, status, xhr){
-				var prices=JSON.parse(js), price=0, obj=null;
-
-				row.addClass('loaded')
-				ptd.removeClass('loading').text('');
-
-				if (prices.error) {
-					ptd.addClass('none').text('none');
-				}
-				else {
-					if (prices.length > 0) {
-						obj = prices[0]
-						price = obj['price'].toFixed(2);
-						ptd.html('<a href="'+obj['url']+'">'+price+'</a>');
-						if (lowest_price == null) {
-							lowest_price = price;
-							lowest_elem  = row;
+			var	updateLowest = function(price, row) {
+					if (lowestPrice == null) {
+						lowestPrice = price;
+						lowestElem  = row;
+					}
+					else {
+						if (price < lowestPrice) {
+							lowestPrice = price;
+							lowestElem  = row;
 						}
-						else {
-							if (price < lowest_price) {
-								lowest_price = price;
-								lowest_elem  = row;
+					}
+				};
+
+			rows.each(function(){
+				var	name = this.className,
+					shopTR = $(this),
+					priceTD = shopTR.find('td.price');
+
+				shopTR.addClass('loading');
+				priceTD.text('loading');
+				book['shop'] = name;
+
+				$.get('/price', book, function(js, status, xhr){
+					var	prices = JSON.parse(js),
+						price = 0;
+
+					shopTR.removeClass('loading');
+					priceTD.text('');
+
+					if (prices.length == 0 || prices.error) {
+						shopTR.addClass('none');
+						priceTD.text('none');
+					}
+					else {
+						shopTR.addClass('priced');
+						if (prices.length == 1) {
+							var obj = prices[0];
+							price = obj.price.toFixed(2);
+							priceTD.html('<a href="'+obj.url+'">'+price+'</a>');
+							updateLowest(price, shopTR);
+						}
+						else if (prices.length > 1) {
+							for (var i=prices.length-1,obj,pRow,date; i>=0; i--) {
+								obj = prices[i];
+								date = [obj.posted.day, obj.posted.month, obj.posted.year].join('/');
+								price = obj.price.toFixed(2);
+								pRow = $('<tr class="'+name+' priced sub"><th scope="row" class="store">'+date+'</th><td class="price"><a href="'+obj.url+'">'+price+'</a></td></tr>');
+								shopTR.after(pRow);
+								updateLowest(price, pRow);
 							}
 						}
 					}
-					else {
-						ptd.addClass('none').text('none');
+
+					loaded = loaded + 1;
+					if (loaded >= total) {
+						completed();
 					}
-				}
+				}, 'application/json');
+			});
 
-				loaded = loaded + 1;
-				if (loaded >= total) {
-					completed();
-				}
-			}, 'application/json');
-		});
-
-		rows.click(function(e){
-			var row = $(this);
-			if (!row.hasClass('loaded') || e.tagName.toUpperCase() == 'A') {
-				return;
-			}
-			var url = row.find('a').attr('href');
-			if (url) {
-				window.location = url;
-			}
-		});
+			//rows.live('click', function(e){
+			//	var row = $(this);
+			//	if (!row.hasClass('loaded') || e.target.tagName.toUpperCase() == 'A') {
+			//		return;
+			//	}
+			//	var url = row.find('a').attr('href');
+			//	if (url) {
+			//		window.location = url;
+			//	}
+			//});
+		}
 	};
 
 })(jQuery);
